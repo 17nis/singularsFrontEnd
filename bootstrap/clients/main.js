01142f659c42c542f117ffc7c2f397001e2d0323
genis <genis.hti@gmail.com>
2020-12-11 14:09:34 +0100

    update 11-12-2020

---
+++ bootstrap/clients/main.js
@@ -0,0 +1,117 @@
+function myFunction() {
+    var image = document.getElementById("image1");
+    var ingredientsList1 = document.getElementById("ingredientsList1");
+    var btnText = document.getElementById("ingredients1");
+    var overflow = document.getElementById("overflow1")
+
+    if (image.style.display === "none") {
+        image.style.display = "inline";
+        btnText.innerHTML = "Ingredients";
+        ingredientsList1.style.display = "none";
+        overflow.style.overflow = "hidden";
+    } else {
+        image.style.display = "none";
+        btnText.innerHTML = "Cake photo";
+        ingredientsList1.style.display = "inline";
+        overflow.style.overflow = "scroll";
+    }
+}
+
+
+function myFunction2() {
+    var image = document.getElementById("image2");
+    var ingredientsList1 = document.getElementById("ingredientsList2");
+    var btnText = document.getElementById("ingredients2");
+    var overflow = document.getElementById("overflow2")
+
+    if (image.style.display === "none") {
+        image.style.display = "inline";
+        btnText.innerHTML = "Ingredients";
+        ingredientsList1.style.display = "none";
+        overflow.style.overflow = "hidden";
+    } else {
+        image.style.display = "none";
+        btnText.innerHTML = "Cake photo";
+        ingredientsList1.style.display = "inline";
+        overflow.style.overflow = "scroll";
+    }
+}
+
+
+function myFunction3() {
+    var image = document.getElementById("image3");
+    var ingredientsList1 = document.getElementById("ingredientsList3");
+    var btnText = document.getElementById("ingredients3");
+    var overflow = document.getElementById("overflow3")
+
+    if (image.style.display === "none") {
+        image.style.display = "inline";
+        btnText.innerHTML = "Ingredients";
+        ingredientsList1.style.display = "none";
+        overflow.style.overflow = "hidden";
+    } else {
+        image.style.display = "none";
+        btnText.innerHTML = "Cake photo";
+        ingredientsList1.style.display = "inline";
+        overflow.style.overflow = "scroll";
+    }
+}
+
+function myFunction4() {
+    var image = document.getElementById("image4");
+    var ingredientsList1 = document.getElementById("ingredientsList4");
+    var btnText = document.getElementById("ingredients4");
+    var overflow = document.getElementById("overflow4")
+
+    if (image.style.display === "none") {
+        image.style.display = "inline";
+        btnText.innerHTML = "Ingredients";
+        ingredientsList1.style.display = "none";
+        overflow.style.overflow = "hidden";
+    } else {
+        image.style.display = "none";
+        btnText.innerHTML = "Cake photo";
+        ingredientsList1.style.display = "inline";
+        overflow.style.overflow = "scroll";
+    }
+}
+
+
+function myFunction5() {
+    var image = document.getElementById("image5");
+    var ingredientsList1 = document.getElementById("ingredientsList5");
+    var btnText = document.getElementById("ingredients5");
+    var overflow = document.getElementById("overflow5")
+
+    if (image.style.display === "none") {
+        image.style.display = "inline";
+        btnText.innerHTML = "Ingredients";
+        ingredientsList1.style.display = "none";
+        overflow.style.overflow = "hidden";
+    } else {
+        image.style.display = "none";
+        btnText.innerHTML = "Cake photo";
+        ingredientsList1.style.display = "inline";
+        overflow.style.overflow = "scroll";
+    }
+}
+
+
+function myFunction6() {
+    var image = document.getElementById("image6");
+    var ingredientsList1 = document.getElementById("ingredientsList6");
+    var btnText = document.getElementById("ingredients6");
+    var overflow = document.getElementById("overflow6")
+
+    if (image.style.display === "none") {
+        image.style.display = "inline";
+        btnText.innerHTML = "Ingredients";
+        ingredientsList1.style.display = "none";
+        overflow.style.overflow = "hidden";
+    } else {
+        image.style.display = "none";
+        btnText.innerHTML = "Cake photo";
+        ingredientsList1.style.display = "inline";
+        overflow.style.overflow = "scroll";
+    }
+}
